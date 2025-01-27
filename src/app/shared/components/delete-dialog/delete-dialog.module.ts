775fb6f1ff0b7e767883198731ac38f13889a181
Aliaksandr Kazhamiaka <aliaksandr.kazhamiaka@gmail.com>
2025-01-27 10:25:29 +0300

    refactor(delete-dialog): update buttons

--- src/app/shared/components/delete-dialog/delete-dialog.module.ts
+++ src/app/shared/components/delete-dialog/delete-dialog.module.ts
@@ -5,15 +5,15 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { TranslateModule } from '@ngx-translate/core';
 import { ButtonModule } from '../button/button.module';
+import { CmButton } from '../button/button';
 
 @NgModule({
   declarations: [DeleteDialogComponent],
   imports: [
     CommonModule,
-    MatButtonModule,
     TranslateModule,
     MatIconModule,
-    ButtonModule,
+    CmButton,
   ],
   exports: [
     DeleteDialogComponent,
